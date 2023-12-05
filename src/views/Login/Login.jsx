7e3eaf351f8d0bcca5f4693e9eb3141b9580f429
FragaGabriel23 <gabrielfraga2308@gmail.com>
2023-12-05 03:26:13 -0300

    Login Stylized

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -6,7 +6,21 @@ import Header from '../../components/Header/Header'
 const Login = () => {
     return (
         <section className='login'>
-            <Header/>
+            <Header />
+            <form className='login__form'>
+                <h1>Acesse com seu login ou cadastre-se!</h1>
+                <h2>você pode entrar com o seu CPF</h2>
+
+                <div className="login__input-wrapper">
+                    <label htmlFor="login">Digite seu Login:</label>
+                    <input type='text' placeholder='Login' name='login' required></input>
+
+                    <label htmlFor="password">Senha:</label>
+                    <input type='password' placeholder='***************' name='password' required></input>
+                </div>
+
+                <button type='submit'>Entrar</button>
+            </form >
         </section>
     )
 }

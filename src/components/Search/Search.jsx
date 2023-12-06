3d8712445f6b4e73b9c33b357687d360373724c7
FragaGabriel23 <gabrielfraga2308@gmail.com>
2023-12-06 03:39:56 -0300

    Add Search Component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './index.scss'
+import search from '../../assets/search.svg'
+import profile from '../../assets/profile.svg'
+import heart from '../../assets/heart.svg'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='Search'>
+      <div className='Search__input'>
+        <img src={search} alt="Search Icon" />
+        <input type="text" name='search' placeholder='O que você está procurando?' />
+      </div>
+      <div className='Search__Icons'>
+        <img src={profile} alt="Profile Icon" />
+        <img src={heart} alt="Heart Icon" />
+      </div>
+    </div>
   )
 }
 

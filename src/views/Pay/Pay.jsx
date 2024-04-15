f09764ae9fc1f3ff9b063dfc6b51564043bb3cb7
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-04-15 11:36:44 -0300

    feat(views/Pay): responsive added

--- src/views/Pay/Pay.jsx
+++ src/views/Pay/Pay.jsx
@@ -7,61 +7,89 @@ const Pay = ({ data }) => {
   const selectedProduct = data.find((product) => product.id == payId);
 
   return (
-    <div>
+    <>
       <Header />
-      <div className="pay-detail">
+
+      <section className="pay-detail">
+
         <div className="pay-detail__left-side">
+
           <form className="pay-detail__form">
+
             <div className="pay-detail__input-wrapper">
-              <label htmlFor="name">Nome:</label>
-              <input
-                type="text"
-                placeholder="Nome completo"
-                id="name"
-                className="pay-detail__input-name"
-              />
-              <label htmlFor="entrega">Endereço de entrega:</label>
-              <input
-                type="text"
-                id="entrega"
-                placeholder="Endereço de entrega"
-              />
+
+              <div>
+                <label htmlFor="name">Nome:</label>
+                <input
+                  type="text"
+                  placeholder="Nome completo"
+                  id="name"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="entrega">Endereço de entrega:</label>
+                <input
+                  type="text"
+                  id="entrega"
+                  placeholder="Endereço de entrega"
+                />
+              </div>
+
             </div>
 
             <div className="pay-detail__format">
+
               <h2>Forma de Pagamento:</h2>
-              <div className="pay-detail__pix">
-                <input type="checkbox" id="pix" name="format" />
+
+              <div className="pay-detail__formatInput">
+                <input type="radio" id="pix" name="format" />
                 <label htmlFor="pix">Pix</label>
               </div>
-              <div className="pay-detail__boleto">
-                <input type="checkbox" id="boleto" name="format" />
+
+              <div className="pay-detail__formatInput">
+                <input type="radio" id="boleto" name="format" />
                 <label htmlFor="boleto">Boleto</label>
               </div>
-              <div className="pay-detail__credit-card">
-                <input type="checkbox" id="credit-card" name="format" />
+
+              <div className="pay-detail__formatInput">
+                <input type="radio" id="credit-card" name="format" />
                 <label htmlFor="credit-card">Cartão de Crédito</label>
               </div>
+
             </div>
 
             <button type="submit">
               <Link to={"/home"}>Enviar Pedido</Link>
             </button>
+
           </form>
+
         </div>
+
         <div className="pay-detail__right-side">
+
           <h1>Resumo do Pedido:</h1>
+
           <div className="pay-detail__product-price">
+
             <div className="pay-detail__description">Produtos (1):</div>
             <h1>{selectedProduct.price}</h1>
+
           </div>
+
           <div className="pay-detail__product-total">
+
             <div className="pay-detail__description">Total:</div>
             <h1>{selectedProduct.price}</h1>
+
           </div>
+
         </div>
-      </div>
-    </div>
+
+      </section>
+
+    </>
   );
 };
 
